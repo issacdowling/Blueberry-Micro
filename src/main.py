e7ec4ba2a1b76b8cf81886c2364efe71caeb2496
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2023-12-26 12:37:13 +0000

    Add TTS test and tts_data to gitignore

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+import subprocess
+import sys
 
-print("blueberry")
+## Test TTS by speaking on launch
+speech_text = "Blueberry is starting up"
+tts_data_dir = "tts_data"
+tts_model = f"{tts_data_dir}/en_US-lessac-high.onnx" # If this is a path, it will be loaded directly, but if it's just the name, it will redownload every time. https://github.com/rhasspy/piper to download.
+
+subprocess.call(f'echo "{speech_text}" | {sys.executable} -m piper --data-dir {tts_data_dir} --download-dir {tts_data_dir} --model {tts_model} --output_file {tts_data_dir}/test.wav', stdout=subprocess.PIPE, shell=True)
+
+
+#STT
+stt_model = "base.en"
