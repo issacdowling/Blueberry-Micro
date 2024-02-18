f2db9aac1767f6180015e1bb85e2996e06c07a19
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-02-18 19:15:40 +0000

    Basic logging added

--- src/blueberry/main.py
+++ src/blueberry/main.py
@@ -7,6 +7,16 @@ import json
 import pathlib
 import os
 import uuid
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
+logging.addLevelName( logging.DEBUG, "\033[96m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
+logging.addLevelName( logging.INFO, "\033[92m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+logging.addLevelName( logging.WARNING, "\033[93m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName( logging.ERROR, "\033[91m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+logging.addLevelName( logging.CRITICAL, "\033[91m%s\033[1;0m" % logging.getLevelName(logging.CRITICAL))
 
 # Load everything ##############################################
 
@@ -14,7 +24,7 @@ import uuid
 data_path = pathlib.Path(os.environ['HOME']).joinpath(".config/bloob")
 server_config = None
 if data_path.exists():
-  print("Loading Config")
+  logging.debug("Loading Config")
   with open(data_path.joinpath("config.json"),"r") as instance_config_json:
     instance_config = json.load(instance_config_json)
   # Attempt to download a device configuration file from the server
@@ -22,17 +32,18 @@ if data_path.exists():
   if instance_config.get("mode") == None or instance_config.get("mode") == "local":
     download = False
   if instance_config.get("server_url") == None and download == True:
-    print("Critical error, invalid server_url defined in the instance configuration file. Please correct this.")
+    logging.critical("Critical error, invalid server_url defined in the instance configuration file. Please correct this.")
     exit(0)
   elif download == True:
     server_config = None
     config_query_uri = f"{instance_config.get('server_url')}/{instance_config.get('uuid')}/config"
-    print(f"Pulling config from: {config_query_uri}")
+    logging.debug(f"Pulling config from: {config_query_uri}")
     server_config = requests.get(config_query_uri).json()
 else:
   # Create configuration directory, add skeleton config file
-  print("Creating Config Directory")
+  logging.debug("Creating Config Directory")
   data_path.mkdir()
+  logging.info("Created Config Directory")
   template_config = {"instance_name":"Default Name","uuid":str(uuid.uuid4()), "mode":"local", "enabled_pretrained_wakewords": ["weather", "jarvis"],"location":{"lat":10,"long":10}, "stt_model":"Systran/faster-distil-whisper-small.en", "tts_model":"en_US-lessac-high","devices": {"wled": {},"tasmota":{}, "http":{}}, "cores": {}}
   with open(data_path.joinpath("config.json"), 'w') as instance_config:
       instance_config.write(json.dumps(template_config))
@@ -42,16 +53,18 @@ if server_config != None:
     devices_json = server_config
 else:
     if(instance_config.get("devices") == None):
-        print("Devices configuration not found. Instance configuration malformed. Exiting.")
+        logging.critical("Devices configuration not found. Instance configuration malformed. Exiting.")
         exit(0)
     devices_json = instance_config.get("devices")
+
+logging.info("Loaded Config")
 
 ## Load Cores / Device Handlers
 cores = []
 
 class Core:
   def __init__(self, path):
-    print(f"Loading: {path}")
+    logging.debug(f"Loading: {path}")
 
     self.path = path
 
@@ -66,9 +79,9 @@ class Core:
     cores.append(self)
 
     if core_json.get("device_handler"):
-      print(f"Loaded Device Handler: {self.name}")
+      logging.info(f"Loaded Device Handler: {self.name}")
     else:
-      print(f"Loaded Core: {self.name}")
+      logging.info(f"Loaded Core: {self.name}")
 
 ### Load cores
 core_path = data_path.joinpath("cores")
@@ -82,8 +95,6 @@ core_files = [str(core) for core in core_path.glob('*bb_core*')]
 for core_file in core_files:
   ## INITs each core, expecting it to return basic info
   Core(path = core_file)
-  
-print(cores)
 
 
 ## Define Devices #######################
@@ -176,7 +187,8 @@ class HTTPDevice(BaseDevice):
 # TODO: Fetch file to define devices from server on start
 
 ## Instantiate all devices
-print("Loading Devices")
+logging.debug("Loading Devices")
+
 for wled_device in devices_json["wled"]:
   WledDevice(devices_json["wled"][wled_device]["names"], devices_json["wled"][wled_device]["IP"])
 for tasmota_device in devices_json["tasmota"]:
@@ -184,6 +196,7 @@ for tasmota_device in devices_json["tasmota"]:
 for http_device in devices_json["http"]:
     HTTPDevice(devices_json["http"][http_device]["names"], devices_json["http"][http_device].get("on_url"), devices_json["http"][http_device].get("off_url"))
 
+logging.info("Loaded Devices")
 ## Load faster-whisper #######################
 
 from faster_whisper import download_model
@@ -193,19 +206,20 @@ stt_path = data_path.joinpath("stt")
 
 stt_model = instance_config["stt_model"]
 
-print(f"Loading Model: {stt_model}")
+logging.debug(f"Loading Model: {stt_model}")
 if not stt_path.exists():
-  print("Creating Speech-To-Text directory")
+  logging.debug("Creating Speech-To-Text directory")
   stt_path.mkdir()
+  logging.info("Created Speech-To-Text directory")
 # Do this so that unfound models are automatically downloaded, but by default we aren't checking remotely at all, and the
 # STT directory doesn't need to be deleted just to automatically download other models
 try:
   model = WhisperModel(model_size_or_path=stt_model, device="cpu", download_root=stt_path, local_files_only = True)
 except: #huggingface_hub.utils._errors.LocalEntryNotFoundError (but can't do that here since huggingfacehub not directly imported)
-  print(f"Downloading Model: {stt_model}")
+  logging.debug(f"Downloading Model: {stt_model}")
   model = WhisperModel(model_size_or_path=stt_model, device="cpu", download_root=stt_path)
 
-print(f"Loaded Model: {stt_model}")
+logging.info(f"Loaded Model: {stt_model}")
 
 ## Load Piper ###############################
 tts_model = instance_config["tts_model"]
@@ -216,7 +230,7 @@ output_speech_wav_path = tts_path.joinpath("output_speech.wav")
 #The path must be converted to a string rather than a posixpath or it breaks python-mpv
 def speak(speech_text, tts_model_path=f"{tts_path}/{tts_model}.onnx", output_audio_path=str(output_speech_wav_path), play_speech=True, blocking=False):					
   subprocess.call(f'echo "{speech_text}" | {sys.executable} -m piper --data-dir {tts_path} --download-dir {tts_path} --model {tts_model_path} --output_file {output_audio_path}', stdout=subprocess.PIPE, shell=True)
-  print(f"Speaking: {speech_text}")
+  logging.info(f"Speaking: {speech_text}")
   if play_speech:
     audio_playback_system.play(output_audio_path)
     if blocking:
@@ -224,10 +238,13 @@ def speak(speech_text, tts_model_path=f"{tts_path}/{tts_model}.onnx", output_aud
 
 # Create directory and download model if necessary
 if not tts_path.exists():
-  print("Creating TTS Data Directory")
+  logging.debug("Creating TTS Data Directory")
   tts_path.mkdir()
-  print("Downloading Model")
+  logging.info("Created TTS Data Directory")
+
+  logging.debug("Downloading TTS Model")
   speak("Download",tts_model_path=tts_model, play_speech=False)
+  logging.debug("Downloaded TTS Model")
 
 ## Load intent parser #######################
 get_keyphrases = ["get", "what", "whats", "is"]
@@ -277,20 +294,21 @@ total_devices = audio_recording_system_info.get("deviceCount")
 for device_index in range(total_devices):
   if audio_recording_system.get_device_info_by_host_api_device_index(0, device_index).get("name") == "pipewire":
     mic_index = audio_recording_system.get_device_info_by_host_api_device_index(0, device_index).get("index")
-print(f"Found pipewire at index {mic_index}")
+logging.info(f"Found pipewire at index {mic_index}")
 
 # Assign and create Wakeword data directory if necessary
 ww_path = data_path.joinpath("ww")
 if not ww_path.exists():
-  print("Creating Wakeword Data Directory")
+  logging.debug("Creating Wakeword Data Directory")
   ww_path.mkdir()
+  logging.info("Created Wakeword Data Directory")
 
 #This is turned into a str because otherwise python-mpv and faster-whisper broke
 detected_speech_wav_path = str(ww_path.joinpath("detected_speech.wav"))
 ## TODO: Eventually get this list from the server
 ## TODO: Allow certain actions to be performed solely from saying certain wakewords (split into "wake"words and "action"words or something)
 ## Loads enabled pretrained models and all .tflite custom models in the wakeword folder
-print(f"Found these OpenWakeWord Models: {[str(model) for model in ww_path.glob('*.tflite')]}")
+logging.info(f"Found these OpenWakeWord Models: {[str(model) for model in ww_path.glob('*.tflite')]}")
 enabled_wakewords = instance_config["enabled_pretrained_wakewords"] + [str(model) for model in ww_path.glob('*.tflite')]
 ## TODO: Add automatically downloading "personal wakewords" from configuration server and enabling them
 
@@ -299,7 +317,7 @@ oww = Model(wakeword_models=enabled_wakewords, vad_threshold=vad_threshold, infe
 speech_buffer = []
 
 ## Open Mic:
-print("Opening Mic")
+logging.info("Opening Mic")
 mic_stream = audio_recording_system.open(format=paInt16, channels=channels, rate=sample_rate, input=True, frames_per_buffer=frame_size, input_device_index=mic_index)
 
 ## Load Intent Parser:
@@ -364,7 +382,7 @@ def turnListeningDeviceOnOff(listening=True):
 from datetime import datetime
 
 ## Detection loop
-print("Waiting for wakeword:")
+logging.debug("Waiting for wakeword:")
 while True:
 
   ## Begin capturing audio
@@ -391,7 +409,7 @@ while True:
       oww.predict(np.zeros(sample_rate*4, np.int16))
       
       # Record the wakeword and the following phrase, stopping when no speech detected
-      print("Recording: waiting for 1s of silence")
+      logging.debug("Recording: waiting for 1s of silence")
       vad_speech_margin = vad_speech_margin_init
       while vad_speech_margin > 0:
         current_frame = np.frombuffer(mic_stream.read(frame_size), dtype=np.int16)
@@ -405,15 +423,19 @@ while True:
           else:
             vad_speech_margin -= 320
 
+      logging.info("Finished Recording")
+
       #Play stopped recording sound:
       audio_playback_system.play("resources/audio/stoplistening.wav")
       turnListeningDeviceOnOff(False)
-      print("Saving Audio")
+      logging.debug("Saving Audio")
       with wave.open(detected_speech_wav_path, 'wb') as wf:
         wf.setnchannels(channels)
         wf.setsampwidth(audio_recording_system.get_sample_size(paInt16))
         wf.setframerate(sample_rate)
         wf.writeframes(b''.join(speech_buffer))
+
+      logging.info("Saved audio")
 
 # STT ########################################################
       # TODO: Send audio to STT directly rather than using a file for it. Still record audio to /dev/shm for option to replay
@@ -421,11 +443,11 @@ while True:
       # across significantly different tiers of hardware
       segments, info = model.transcribe(detected_speech_wav_path, beam_size=5, condition_on_previous_text=False) #condition_on_previous_text=False reduces hallucinations and inference time with no downsides for our short text.
 
-      print("Transcribing...")
+      logging.debug("Transcribing...")
       raw_spoken_words = ""
       for segment in segments:
         raw_spoken_words += segment.text
-      print("Transcribed words:", raw_spoken_words)
+      logging.info(f"Transcribed words: {raw_spoken_words}")
       
 # Word preprocessing ###########################################
       raw_spoken_words_list = raw_spoken_words.split(" ")
@@ -440,8 +462,7 @@ while True:
       spoken_words = re.sub('[^A-Za-z0-9 ]+', "", spoken_words).lower()
 
       spoken_words_list = spoken_words.split(" ")
-      print("Cleaned up words:", spoken_words)
-
+      logging.info(f"Cleaned up words: {spoken_words}")
 # Intent Recognition ###########################################
 
       if len(spoken_words) == 0:
@@ -470,17 +491,15 @@ while True:
               spoken_state = spoken_states[index]
             except IndexError:
               pass
-
             # Match case not used because colours / brightness make it less good
             ### Set the state ##################
             try:
-                print(f"Turning {device.friendly_name} {spoken_state}" )
+                logging.info(f"Turning {device.friendly_name} {spoken_state}" )
                 #Boolean
                 if spoken_state == "on":
                   device.on()
                 elif spoken_state == "off":
                   device.off()
-                  print(device.friendly_name)
                 # Colours / custom states
                 elif spoken_state in state_colour_keyphrases:
                   device.setColour(colours_json["rgb"][spoken_state])
@@ -546,4 +565,4 @@ while True:
 
 
 # Back to beginning
-      print("Waiting for wakeword:")
+      logging.debug("Waiting for wakeword:")
