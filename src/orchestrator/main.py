d5b0ea12f25f315788024d82a1f640bdea0c2936
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-03-25 23:04:34 +0000

    Add orchestrator intent, core, tts, and audio playback

--- src/orchestrator/main.py
+++ src/orchestrator/main.py
@@ -9,7 +9,6 @@ import paho.mqtt.subscribe as subscribe
 import pathlib
 from random import randint
 
-
 def exit_cleanup():
   for core in loaded_cores:
     list(core.values())[0].kill()
@@ -29,13 +28,15 @@ parent_folder_dir = pathlib.Path(__file__).parents[1]
 
 util_files = []
 util_files.extend([parent_folder_dir.joinpath("utils/audio_playback/main.py"), parent_folder_dir.joinpath("utils/audio_recorder/main.py"), parent_folder_dir.joinpath("utils/stt/main.py"), parent_folder_dir.joinpath("utils/tts/main.py"), parent_folder_dir.joinpath("utils/wakeword/main.py"), parent_folder_dir.joinpath("utils/intent_parser/main.py")])
-external_core_files = [str(core) for core in cores_dir.glob('*bb_core*')]
+external_core_files = [str(core) for core in cores_dir.glob('**/*bb_core*')]
+internal_core_files = [str(core) for core in parent_folder_dir.glob('**/*bb_core*')]
 loaded_utils = []
 loaded_cores = []
 
 
 for util_file in util_files:
   print(f"Attempting to load {util_file}")
+  ## This fails if the underlying core crashes, often caused by not having the venv activated and therefore missing imports
   util_run = subprocess.run([util_file, "--identify", "true"],capture_output=True)
   util_json = json.loads(util_run.stdout.decode())
   loaded_utils.append({util_json["id"]: subprocess.Popen([util_file, "--host", config_json["mqtt"]["host"], "--port", str(config_json["mqtt"]["port"]), "--device-id", config_json["uuid"]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)})
@@ -43,7 +44,7 @@ for util_file in util_files:
   print(f"Loaded Util: {util_json['id']}")
 
 
-for core_file in external_core_files:
+for core_file in external_core_files + internal_core_files:
   print(f"Attempting to load {core_file}")
   core_run = subprocess.run([core_file, "--identify", "true"],capture_output=True)
   core_json = json.loads(core_run.stdout.decode())
@@ -73,14 +74,25 @@ while True:
   publish.single(f"bloob/{config_json['uuid']}/stt/transcribe", payload=json.dumps({"id": request_identifier, "audio": recording}), hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"])
   #Receive transcription
   transcript = json.loads(subscribe.simple(f"bloob/{config_json['uuid']}/stt/finished", hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"]).payload)["text"]
-  print(f"Transcription finished - {transcript} - starting intent recognition")
+  print(f"Transcription finished - {transcript} - sending to intent parser")
 
   #Parse
   publish.single(f"bloob/{config_json['uuid']}/intent_parser/run", payload=json.dumps({"id": request_identifier, "text": transcript}), hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"])
-  print("sent to parser, waiting to receieve")
   #Receieve parsed intent
   parsed_json = json.loads(subscribe.simple(f"bloob/{config_json['uuid']}/intent_parser/finished", hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"]).payload.decode())
-  
-  print(transcript, parsed_json["intent"], parsed_json["core"])
+  print(f"Parsing finished - sending to core")
 
-input()
+  #Fire intent to relevant core
+  publish.single(f"bloob/{config_json['uuid']}/cores/{parsed_json['core_id']}/run", payload=json.dumps({"id": request_identifier, "intent": parsed_json['intent'], "text": parsed_json['text']}), hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"], client_id="bloob-orchestrator")
+  #Get output from the core
+  core_json = json.loads(subscribe.simple(f"bloob/{config_json['uuid']}/cores/{parsed_json['core_id']}/finished", hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"], client_id="bloob-orchestrator").payload.decode())
+  print(f"Core finished - sending to TTS")
+
+  #Text to speech the core's output
+  publish.single(f"bloob/{config_json['uuid']}/tts/run", payload=json.dumps({"id": request_identifier, "text": core_json["speech"]}), hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"])
+  #Get TTS output
+  tts_audio = json.loads(subscribe.simple(f"bloob/{config_json['uuid']}/tts/finished", hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"]).payload.decode())["audio"]
+  print(f"TTS finished - sending to audio_playback")
+
+  #Send to audio playback util
+  publish.single(f"bloob/{config_json['uuid']}/audio_playback/run", payload=json.dumps({"id": request_identifier, "audio": tts_audio}), hostname=config_json["mqtt"]["host"], port=config_json["mqtt"]["port"])
