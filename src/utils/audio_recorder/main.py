3d21b48280df9eb4a2b6bdff175ba93d134e8178
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-03-31 20:57:58 +0100

    Increase VAD aggressiveness

--- src/utils/audio_recorder/main.py
+++ src/utils/audio_recorder/main.py
@@ -51,7 +51,7 @@ sample_rate = 16000 # Also saves data, though it may be changed in the future
 frame_size = 1280 # This value chosen because oww recommends 80ms frames, 16000/1280 = 12.5, 1000/12.5 = 80ms, and I just didn't change it for the recorder
 vad_speech_margin_init = 12000 # The number of samples (normally 16000 for 1s) of "Not Speech" before the recording stops
 
-vad_aggressiveness = 2 # 0-3, least to most aggressive at filtering noise
+vad_aggressiveness = 3 # 0-3, least to most aggressive at filtering noise
 
 from pyaudio import PyAudio, paInt16
 audio_recording_system = PyAudio()
