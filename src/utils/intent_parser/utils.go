5217ed1095b3ab373b3a6303fc0617672e2dafe2
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-05-29 00:29:29 +0100

    Add bTextReplace function for replacing only whole words where needed

--- src/utils/intent_parser/utils.go
+++ src/utils/intent_parser/utils.go
@@ -26,3 +26,28 @@ func bTextMatches(text string, checks []string) map[int]string {
 
 	return matches
 }
+
+func bTextReplace(text string, oldPhrase string, newPhrase string) (string, bool) {
+	changeMade := false
+	// If the check is one word, check for whole-word matches, multi-word gets regular .Contains
+	untouchedText := text
+	if len(strings.Split(oldPhrase, " ")) == 1 {
+		var tempText string
+		for _, word := range strings.Split(text, " ") {
+			if word == oldPhrase {
+				tempText = strings.Join([]string{tempText, newPhrase}, " ")
+			} else {
+				tempText = strings.Join([]string{tempText, word}, " ")
+			}
+		}
+		text = tempText
+	} else {
+		text = strings.Replace(text, oldPhrase, newPhrase, -1)
+	}
+
+	if untouchedText != text {
+		changeMade = true
+	}
+
+	return text, changeMade
+}
