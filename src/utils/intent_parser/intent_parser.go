86994f1c17e8b41442f900b03369139abc79d248
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-05-29 01:02:13 +0100

    Add function to perform all necessary keyphrase substitutions

--- src/utils/intent_parser/intent_parser.go
+++ src/utils/intent_parser/intent_parser.go
@@ -16,14 +16,18 @@ type Intent struct {
 	CollectionIds [][]string `json:"collections"`
 	Prefixes      []string
 	Suffixes      []string
+	Variables     []map[string]interface{}
 }
 
 type Collection struct {
+	Id         string
+	Keyphrases []map[string]string
+	Variables  []map[string]interface{}
 }
 
-var intents []Intent
+var intents map[string]Intent = make(map[string]Intent)
 
-var collections []Collection
+var collections map[string]Collection = make(map[string]Collection)
 
 func main() {
 	// Test data
@@ -32,7 +36,7 @@ func main() {
 		"id": "setWLED",
 		"keyphrases": [{"hello there": "hi", "hey": ""}, {"time": "1", "times": "2"}],
 		"core_id": "wled",
-		"collections": [["colours", "boolean"], ["set"]],
+		"collections": [["devices"], ["set"]],
 		"prefixes": ["ask wled to", "do"],
 		"suffixes": ["thanks", "or else"]
 	}	
@@ -44,7 +48,30 @@ func main() {
 		log.Panic(err)
 	}
 
+	testCollection := []byte(`{
+	"id": "devices",
+	"keyphrases": [
+		{"doorlight": "door light"},
+		{"door light": ""},
+		{"doleight": "door light"},
+		{"delight": "door light"}
+		],
+	"variables": [
+		{"door light": [0,233234,232443]}
+	]
+	}	
+	`)
+
+	var testCollectionJson Collection
+
+	err = json.Unmarshal(testCollection, &testCollectionJson)
+	if err != nil {
+		log.Panic(err)
+	}
+	collections[testCollectionJson.Id] = testCollectionJson
+
 	fmt.Println(testDataJson)
+	fmt.Println(testCollectionJson)
 
 	fmt.Println(parseIntent(preCleanText("ask wled to hello there, time, thanks"), []Intent{testDataJson}))
 
@@ -120,11 +147,39 @@ func parseIntent(text string, intents []Intent) ([]Intent, string) {
 	return potentialIntents, ""
 }
 
-func postProcessText(text string, intent Intent) string {
+func preProcessText(text string, intent Intent) string {
 
+	// Definitely feels like all of these for loops should be somehow optimisable
+	if intent.CollectionIds != nil {
+		for _, setOfCollections := range intent.CollectionIds {
+			for _, collectionId := range setOfCollections {
+				if collection, ok := collections[collectionId]; ok {
+					if collection.Keyphrases != nil {
+						for _, setOfKeyphrases := range collection.Keyphrases {
+							for keyphrase, newphrase := range setOfKeyphrases {
+								text, _ = bTextReplace(text, keyphrase, newphrase)
+							}
+						}
+					}
+				} else {
+					fmt.Printf("The Collection \"%s\" doesn't exist, but was called for by %s", collectionId, intent.Id)
+				}
+			}
+
+		}
+	}
+
+	if intent.Keyphrases != nil {
+		for _, setOfKeyphrases := range intent.Keyphrases {
+			for keyphrase, newphrase := range setOfKeyphrases {
+				text, _ = bTextReplace(text, keyphrase, newphrase)
+			}
+		}
+
+	}
 	// Do the same for keyphrases in Collections eventually
 
-	return text
+	return strings.TrimSpace(text)
 
 }
 
@@ -148,5 +203,5 @@ func preCleanText(text string) string {
 	// Remove any non-replaced special characters
 	text = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(text, "")
 
-	return text
+	return strings.TrimSpace(text)
 }
