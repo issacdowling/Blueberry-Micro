fc9a89794840fa86715c2aec4ed804b8c1a0725d
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-06-12 23:03:48 +0100

    (awkwardly) prevent music Core from hanging on lack of network connection

--- src/cores/music_mopidy/music_mopidy_bb_core.py
+++ src/cores/music_mopidy/music_mopidy_bb_core.py
@@ -132,93 +132,102 @@ c.publishAll()
 while True:
   c.log("Waiting for input...")
   request_json = c.waitForCoreCall()
-  match request_json["intent"]:
-    case "getCurrentSong":
-      mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.get_current_track"}).text)
-      if mopidy_response_json["result"] == None:
-        c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core found that no song is playing")  
-      else:
-        current_song = mopidy_response_json["result"]["name"]
-        current_artist = mopidy_response_json["result"]["artists"][0]["name"]
-        c.publishCoreOutput(request_json["id"], f"The current song is {current_song} by {current_artist}", f"The Music (Mopidy) Core got that the current song is {current_song} by {current_artist}")
 
-    case "pausePlayback":
-      match getPlaybackState():
-        case "paused":
-          c.publishCoreOutput(request_json["id"], f"I'll unpause the music", f"The Music (Mopidy) Core unpaused the music")
-          mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.resume"}).text)
-        case "playing":
-          c.publishCoreOutput(request_json["id"], f"I'll pause the music", f"The Music (Mopidy) Core paused the music")
-          mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.pause"}).text)
-        case "stopped":
-          c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to pause the music, but nothing is playing")
+  try:
+    match request_json["intent"]:
+      case "getCurrentSong":
+        mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.get_current_track"}).text)
+        if mopidy_response_json["result"] == None:
+          c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core found that no song is playing")  
+        else:
+          current_song = mopidy_response_json["result"]["name"]
+          current_artist = mopidy_response_json["result"]["artists"][0]["name"]
+          c.publishCoreOutput(request_json["id"], f"The current song is {current_song} by {current_artist}", f"The Music (Mopidy) Core got that the current song is {current_song} by {current_artist}")
 
-    case "stopPlayback":
-      match getPlaybackState():
-        case "paused" | "playing":
-          mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.stop"}).text)
-          c.publishCoreOutput(request_json["id"], f"I'll stop the music", f"The Music (Mopidy) Core stopped the music")
-        case _:
-          c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to stop the music, but nothing is playing")
+      case "pausePlayback":
+        match getPlaybackState():
+          case "paused":
+            c.publishCoreOutput(request_json["id"], f"I'll unpause the music", f"The Music (Mopidy) Core unpaused the music")
+            mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.resume"}).text)
+          case "playing":
+            c.publishCoreOutput(request_json["id"], f"I'll pause the music", f"The Music (Mopidy) Core paused the music")
+            mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.pause"}).text)
+          case "stopped":
+            c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to pause the music, but nothing is playing")
 
-    case "nextTrack":
-      match getPlaybackState():
-        case "paused" | "playing":
-          mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.next"}).text)
-          c.publishCoreOutput(request_json["id"], f"I'll skip this track", f"The Music (Mopidy) Core skipped to the next track")
-        case _:
-          c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to skip the music, but nothing is playing")
+      case "stopPlayback":
+        match getPlaybackState():
+          case "paused" | "playing":
+            mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.stop"}).text)
+            c.publishCoreOutput(request_json["id"], f"I'll stop the music", f"The Music (Mopidy) Core stopped the music")
+          case _:
+            c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to stop the music, but nothing is playing")
 
-    case "prevTrack":
-      match getPlaybackState():
-        case "paused" | "playing":
-          mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.previous"}).text)
-          c.publishCoreOutput(request_json["id"], f"I'll go back a track", f"The Music (Mopidy) Core went back to the previous track")
-        case _:
-          c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to go back a track, but nothing is playing")
+      case "nextTrack":
+        match getPlaybackState():
+          case "paused" | "playing":
+            mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.next"}).text)
+            c.publishCoreOutput(request_json["id"], f"I'll skip this track", f"The Music (Mopidy) Core skipped to the next track")
+          case _:
+            c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to skip the music, but nothing is playing")
 
-    case "playTrack":
-      
-      query_text = request_json["text"].replace("play", "")
-      c.log(f"Searching for {query_text} to play")
-      match search_source:
-        case None:
-          # Query _is_ supposed to be a list of strings... even if it's just one string.
-          query_json = {"jsonrpc": "2.0", "id": 1, "method": "core.library.search", "params": {"query": {"track_name": [query_text]}}}
-          mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json=query_json).text)
-          if mopidy_response_json['result'][0].get("tracks") == None:
-            c.publishCoreOutput(request_json["id"], f"I couldn't find any song by the name {query_text}", f"The Music (Mopidy) Core couldn't find a song named {query_text}")
-          else:
-            found_track = mopidy_response_json['result'][0]['tracks'][0]
-            track_name = found_track['name']
-            track_artist = "Unknown artist" if found_track.get("artists") == None or found_track["artists"][0].get("name") == None else found_track["artists"][0]["name"]
-            track_uri = found_track["uri"]
+      case "prevTrack":
+        match getPlaybackState():
+          case "paused" | "playing":
+            mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.previous"}).text)
+            c.publishCoreOutput(request_json["id"], f"I'll go back a track", f"The Music (Mopidy) Core went back to the previous track")
+          case _:
+            c.publishCoreOutput(request_json["id"], f"Nothing is playing right now", f"The Music (Mopidy) Core was asked to go back a track, but nothing is playing")
 
-        case "jf":
-          ## Use fuzzywuzzy to search the complete song list, set the URI to match what Mopidy is expecting
-          fuzzy_result = process.extractOne(query_text, list(jf_songs.keys()), scorer=fuzz.token_sort_ratio)
-          track_index, fuzzy_confidence = fuzzy_result[0], fuzzy_result[1]
-          track_name = jf_songs[track_index]["Name"]
-          track_artist = "Unknown Artist" if jf_songs[track_index].get("AlbumArtist") == None else jf_songs[track_index]["AlbumArtist"]
-          track_uri = f"jellyfin:track:{jf_songs[track_index]['Id']}"
-          c.log(f"Found track {track_name} by {track_artist} with a match of {fuzzy_confidence}%")
+      case "playTrack":
+        
+        query_text = request_json["text"].replace("play", "")
+        c.log(f"Searching for {query_text} to play")
+        match search_source:
+          case None:
+            # Query _is_ supposed to be a list of strings... even if it's just one string.
+            query_json = {"jsonrpc": "2.0", "id": 1, "method": "core.library.search", "params": {"query": {"track_name": [query_text]}}}
+            mopidy_response_json = json.loads(requests.post(f"{base_url}/mopidy/rpc", json=query_json).text)
+            if mopidy_response_json['result'][0].get("tracks") == None:
+              c.publishCoreOutput(request_json["id"], f"I couldn't find any song by the name {query_text}", f"The Music (Mopidy) Core couldn't find a song named {query_text}")
+            else:
+              found_track = mopidy_response_json['result'][0]['tracks'][0]
+              track_name = found_track['name']
+              track_artist = "Unknown artist" if found_track.get("artists") == None or found_track["artists"][0].get("name") == None else found_track["artists"][0]["name"]
+              track_uri = found_track["uri"]
 
-        case "local":
-          fuzzy_result = process.extractOne(query_text, list(local_songs.keys()), scorer=fuzz.token_sort_ratio)
-          track_index, fuzzy_confidence = fuzzy_result[0], fuzzy_result[1]      
-          track_uri = local_songs[track_index]
-          # Search mopidy for metadata later
-          track_name = "test"
-          track_artist = "test"
+          case "jf":
+            ## Use fuzzywuzzy to search the complete song list, set the URI to match what Mopidy is expecting
+            fuzzy_result = process.extractOne(query_text, list(jf_songs.keys()), scorer=fuzz.token_sort_ratio)
+            track_index, fuzzy_confidence = fuzzy_result[0], fuzzy_result[1]
+            track_name = jf_songs[track_index]["Name"]
+            track_artist = "Unknown Artist" if jf_songs[track_index].get("AlbumArtist") == None else jf_songs[track_index]["AlbumArtist"]
+            track_uri = f"jellyfin:track:{jf_songs[track_index]['Id']}"
+            c.log(f"Found track {track_name} by {track_artist} with a match of {fuzzy_confidence}%")
 
-      ## Add track to the tracklist
-      query_json = {"jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add", "params": {"uris": [track_uri]}}
-      track_add_response = json.loads(requests.post(f"{base_url}/mopidy/rpc", json=query_json).text)
+          case "local":
+            fuzzy_result = process.extractOne(query_text, list(local_songs.keys()), scorer=fuzz.token_sort_ratio)
+            track_index, fuzzy_confidence = fuzzy_result[0], fuzzy_result[1]      
+            track_uri = local_songs[track_index]
+            # Search mopidy for metadata later
+            track_name = "test"
+            track_artist = "test"
 
-      ## Play the song at the tlid (tracklist ID?) of the song(s) we just added
-      query_json = {"jsonrpc": "2.0", "id": 1, "method": "core.playback.play", "params": {"tlid": track_add_response["result"][0]["tlid"]}}
-      play_tlid_response = json.loads(requests.post(f"{base_url}/mopidy/rpc", json=query_json).text)
+        ## Add track to the tracklist
+        query_json = {"jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add", "params": {"uris": [track_uri]}}
+        track_add_response = json.loads(requests.post(f"{base_url}/mopidy/rpc", json=query_json).text)
 
-      c.publishCoreOutput(request_json["id"], f"I'll play {track_name} by {track_artist}", f"The Music (Mopidy) Core started playing {track_name} by {track_artist}")
+        ## Play the song at the tlid (tracklist ID?) of the song(s) we just added
+        query_json = {"jsonrpc": "2.0", "id": 1, "method": "core.playback.play", "params": {"tlid": track_add_response["result"][0]["tlid"]}}
+        play_tlid_response = json.loads(requests.post(f"{base_url}/mopidy/rpc", json=query_json).text)
 
+        c.publishCoreOutput(request_json["id"], f"I'll play {track_name} by {track_artist}", f"The Music (Mopidy) Core started playing {track_name} by {track_artist}")
 
+  except (TimeoutError, ConnectionError):
+    to_speak = "I couldn't contact the music service, check your internet connection"
+    explanation = "The Music Core failed to access the mopidy server. The user's internet connection may be down, or the Mopidy server may be down"
+    c.publishCoreOutput(request_json["id"], to_speak, explanation)
+  except:
+    to_speak = "I couldn't access your music, and I'm not sure why"
+    explanation = "The Music Core failed to access the music for an unknown reason"   
+    c.publishCoreOutput(request_json["id"], to_speak, explanation)
