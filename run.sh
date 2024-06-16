bb51435ce2da19eb28f4760c89cdba8ed85562c4
Issac Dowling <12268666-issacdowling@users.noreply.gitlab.com>
2024-06-16 14:04:58 +0000

    Remove utils.go from run/build file

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ echo "If you don't have your Python Venv activated, this will silently fail!"
 echo "If you don't have your MQTT Broker (likely Mosquitto) running, this will not-silently fail!"
 
 cd src/orchestrator
-go build -o orchestrator orchestrator.go mqtt.go utils.go cores.go
+go build -o orchestrator orchestrator.go mqtt.go cores.go
 
 cd ../utils/intent_parser
-go build -o intent_parser_util_bb_core intent_parser_util.go checks.go mqtt.go utils.go
+go build -o intent_parser_util_bb_core intent_parser_util.go checks.go mqtt.go
 
 cd ../../../
 
